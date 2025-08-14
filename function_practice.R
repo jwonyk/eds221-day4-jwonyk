b6540b82c6c67210ec8ea4e76ed47246e5ae9a17
jwonyk <wonyoungkim@ucsb.edu>
2025-08-14 11:59:02 -0700

    lunch time

---
+++ function_practice.R
@@ -0,0 +1,81 @@
+# add up the number of birds and dogs
+
+# define function
+birddog_sum <- function(bird, dog) {
+  
+  pets <- bird + dog
+  return(pets)
+  
+}
+
+
+total_pets <- birddog_sum(bird = 2, dog = 5)
+total_pets <- birddog_sum(2, 5) # same as above
+
+# create a function to double values
+double_it <- function(x) {
+  
+  print(2 * x)
+  
+}
+
+double_it(11)
+
+# write a function with conditionals
+# example is converting animals' ages
+
+animal_age <- function(animal, age) {
+  
+  if(animal == "dog") {
+    
+    print(age * 7)
+    
+  } else if(animal == "goat") {
+    
+    print(age * 4.7)
+    
+  }
+  
+}
+
+# try using for an 8 year old dog
+animal_age(animal = "dog", age = 8)
+
+# try using for a cow
+animal_age(animal = "cow", age = 8)
+
+# write updated version of the animal age function with error messages
+
+animal_age_stop <- function(animal, age) {
+  
+  if(!animal %in% c("dog", "goat")) {
+    
+    stop("Oops! Animal must be a dog or goat.")
+    
+  }
+  
+  if(is.numeric(age) == FALSE) {
+    
+    stop("The age must be a number.")
+    
+  }
+  
+  if(age <= 0 | age > 50) {
+    
+    warning("Are you sure about your animal's age?")
+    
+  }
+  
+  if(animal == "dog") {
+    
+    print(age * 7)
+    
+  } else if(animal == "goat") {
+    
+    print(age * 4.7)
+    
+  }
+  
+}
+
+animal_age_stop("elephant", 10)
